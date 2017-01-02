baf2a2545de1e42f5933b249a3ae483325eba803
Kevin Schlosser <drschlosser@hotmail.com>
2017-01-01 18:35:53 -0700

    Updates eg.Config and eg.PersistentData
    Changes the roll of eg.Config. instead of it being the main handler for the ConfigData it acts more like a subclass of PersistentData.
    
    Created eg.PersistentData.PersistentDataBase which is what an eg.PersistentData subclass becomes. instead of eg.Config.Section
    
    Added extra functionality to the base class. if you iterate through the class it yields key, value pares that it has stored skipping any keys that start with underscore. helps to minimize the code footprint. and also remove the use of iteritems as this is no longer available in python 3
    
    Has each instance of the base handle it's own writing of information. and added checking to see if the class is to be deleted or if the class was loaded only to satisfy the requirements of debugging and can be removed on close.
    
    moved any class checking into the base class under __setattr__ in the event a class gets added dynamically this will change that class to an instance of the base class. as this is a container for settings. it should not have any class instances stored in it dynamically.
    
    Fixes issue when a plugin exists and is installed If the developer added a new attribute to the class and the plugin is upgraded it would not set the new attribute into place and when the attribute would get accessed from the plugin a traceback would occur. Unfortunatly due to having to iterate through each class and add any attributes that do not exist. this adds about 100ms to EG's startup time.
    
    Added formatting of data where the line length is to long. or is a list or tuple that is to long for one line.
    
    It now writes all configuration data to the config.py. not just the values that have changed. This was done because of possible changes to default values in the future. if EG is upgraded all the proper settings can be carried over to the new version regargless of any changes made to the default values.
    
    The big goal to this was to change how the PersistentData and Config work in order to move them to the top of the Core.py file. by doing this the debugging level can get set one time depending on a command line switch or a svaed value in the config.py.

--- eg/Classes/PersistentData.py
+++ eg/Classes/PersistentData.py
@@ -17,7 +17,146 @@
 # with EventGhost. If not, see <http://www.gnu.org/licenses/>.
 
 # Local imports
+
+from types import ClassType, InstanceType
+
 import eg
+
+
+def FormatAttr(attrName, attr, indent):
+    indent4 = ' ' * (indent + 1)
+    indent = ' ' * indent
+    attrField = '%s%s = ' % (indent, attrName)
+
+    multiLine = len(attrField + repr(attr)) > 76
+
+    if multiLine and type(attr) in (unicode, str, list, tuple):
+        if type(attr) == list:
+            attrField += '[\n'
+            for item in attr:
+                attrField += '%s%r,\n' % (indent4, item)
+            attrField = '%s\n%s]' % (attrField[:-2], indent)
+
+        else:
+            if type(attr) in (unicode, str):
+                stop = 76 - len(indent4)
+                if stop > 25:
+                    attrField += '(\n'
+                    while len(repr(attr) + indent4) > 76:
+                        attrField += '%s%r\n' % (indent4, attr[:stop])
+                        attr = attr[stop:]
+                    attrField += '%s%r\n%s)' % (indent4, attr, indent)
+                else:
+                    attrField += repr(attr)
+            else:
+                for item in attr:
+                    attrField += '%s%r,\n' % (indent4, item)
+                attrField = '%s\n%s)' % (attrField[:-2], indent)
+    else:
+        attrField += repr(attr)
+    return attrField + '\n'
+
+
+class PersistentDataBase(object):
+    def __init__(self, parent, key):
+        self._parent = parent
+        self._key = key
+        self._moduleName = repr(parent).split(' ')[0][1:]
+        self._deleteFlag = False
+        self._debugFlag = False
+        if key:
+            self._moduleName += '.' + key
+
+    def SetDelete(self, flag=True):
+        self._deleteFlag = flag
+
+    def SetDebug(self, flag=True):
+        self._debugFlag = flag
+
+    def SaveData(self, fileWriter, indent):
+        classKeys = []
+
+        if self._debugFlag or self._deleteFlag:
+            return
+
+        for key, value in self:
+            if type(value) in (PersistentDataMeta, PersistentDataBase):
+                classKeys.append([key, value])
+            else:
+                line = FormatAttr(key, value, indent)
+                fileWriter(line)
+
+        for key, value in classKeys:
+
+            formatString = '\n%sclass %s:\n'
+            if not indent and key == 'eg':
+                formatString = '\n' + formatString
+
+            fileWriter(formatString % (' ' * indent, key))
+            value.SaveData(fileWriter, indent + 1)
+
+    def __repr__(self):
+        objRepr = repr(self._parent).split(' ')
+        objRepr[0] = '<' + self._moduleName
+        return ' '.join(objRepr)
+
+    def __delete__(self, instance):
+        if self._key:
+            delattr(self._parent, self._key)
+
+    def __iter__(self):
+        for key in sorted(self.__dict__.keys()):
+            if key.startswith('_'):
+                continue
+            yield key, self.__dict__[key]
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def __getattr__(self, item):
+        if item.startswith('_'):
+            pass
+        elif item in self.__dict__:
+            return self.__dict__[item]
+
+        raise AttributeError(
+            '%s does not have attribute %s' % (self._moduleName, item)
+        )
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __setattr__(self, key, value):
+        if key.startswith('_'):
+            object.__setattr__(self, key, value)
+        else:
+            clsTypes = (
+                PersistentDataBase, PersistentDataMeta, ClassType, InstanceType
+            )
+            if type(value) in clsTypes:
+                if hasattr(self, key):
+                    cls = getattr(self, key)
+                else:
+                    cls = PersistentDataBase(self, key)
+
+                for attrName in value.__dict__.keys():
+                    if not attrName.startswith('_'):
+                        setattr(cls, attrName, value.__dict__[attrName])
+                value = cls
+
+            self.__dict__[key] = value
+
+    def __delitem__(self, key):
+        delattr(self, key)
+
+    def __delattr__(self, item):
+        if item in self.__dict__:
+            del (self.__dict__[item])
+        else:
+            raise AttributeError(
+                '%s does not have attribute %s' % (self._moduleName, item)
+            )
+
 
 class PersistentDataMeta(type):
     def __new__(mcs, name, bases, dct):
@@ -27,8 +166,18 @@ class PersistentDataMeta(type):
             config = eg.config
             parts = searchPath.split(".")
             for part in parts[:-1]:
-                config = config.SetDefault(part, eg.Bunch)
-            return config.SetDefault(parts[-1], cls)
+                if hasattr(config, part):
+                    config = getattr(config, part)
+                else:
+                    newConfig = PersistentDataBase(config, part)
+                    setattr(config, part, newConfig)
+                    config = newConfig
+                    if eg.debugLevel:
+                        config.SetDebug()
+
+            setattr(config, parts[-1], cls)
+            return getattr(config, parts[-1])
+
         return cls
 
 
