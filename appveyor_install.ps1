1050d1ff8dccc5a8f6380e33aeda01d3a57d78a7
Kevin Schlosser <drschlosser@hotmail.com>
2018-10-10 23:01:52 -0600

    another try

--- appveyor_install.ps1
+++ appveyor_install.ps1
@@ -2,7 +2,7 @@
 $SysWOWDLL = "$Env:SYSTEMROOT\SysWOW64\python27.dll"
 $SystemDLL = "$Env:SYSTEMROOT\System\python27.dll"
 
-
+$Env:PATH = $Env:PATH -replace "C:\Python27", $Env:PYTHON
 
 If (Test-Path $SystemDLL) {
     Remove-Item $SystemDLL
@@ -23,8 +23,6 @@ if (-Not (Test-Path $Env:PYTHON)) {
         New-Item $InstallersFolder -type directory | Out-Null
     }
 
-
-
     # I am using the VS 2017 appveyor image and this is not installed
     # with that image. It is needed to compile the crypto library
     $VCInstaller = $InstallersFolder + "VCForPython27.msi"
@@ -42,15 +40,64 @@ if (-Not (Test-Path $Env:PYTHON)) {
 
     $SitePackages = "$Env:PYTHON\Lib\site-packages"
 
-    Function PipInstall ($msg, $mod, $log) {
-        $StdErr = $log + "\$msg.err.log"
-        $StdOut = $log + "\$msg.out.log"
+    Function RUN-APP {
+        [CmdletBinding()]
+        Param (
+           [Parameter(Mandatory=$True)]
+           [String]$Executable,
+           [Parameter(Mandatory=$True)]
+           [String]$Args,
+           [Parameter(Mandatory=$False)]
+           [String]$StdErr,
+           [Parameter(Mandatory=$False)]
+           [String]$StdOut,
+           [Parameter(Mandatory=$False)]
+           [String]$MsiFile,
+           [Parameter(Mandatory=$False)]
+           [String]$LogDir
+        )
 
-    
-        Write-Host "  ---- Installing $msg $Env:BUILDARCH" 
-        START-PROCESS pip -RedirectStandardError "$StdErr" -RedirectStandardOutput "$StdOut" -ArgumentList "install", "--no-cache-dir", "$mod"  -NoNewWindow -Wait
-        Write-Host "       Done."
+        if ($LogDir) {
+            $msg = $Args
+            $mod = $StdErr
+
+            $StdErr = "$LogDir\$msg.err.log"
+            $StdOut = "$LogDir\$msg.out.log"
+            $Args = "install --no-cache-dir $mod"
+
+            Write-Host "  ---- Installing $msg $Env:BUILDARCH"
+        }
+
+        if ($StdErr) {
+            Start-Process $Executable -RedirectStandardError $StdErr -RedirectStandardOutput $StdOut -ArgumentList $Args -NoNewWindow -Wait
+        }
+        elseif ($Executable -contains '*.msi') {
+            Start-Process MsiExec.exe -ArgumentList "/I $Executable /quiet /passive /qn /norestart $Args" -NoNewWindow -Wait
+        }
+        else {
+            Start-Process $Executable -ArgumentList "/VerySilent /NoRestart /NoCancel /SupressMessageBoxes /Silent $Args" -NoNewWindow -Wait
+        }
+
+        if ($LASTEXITCODE -eq 0) {
+            Write-Host "       Done."
+            $host.SetShouldExit(0)
+        } else {
+            Write-Host "       Failed."
+            if ($StdErr)  {
+                Write-Host " "
+                Write-Host "******************* ERROR LOG ***********************"
+                Write-Host " "
+                Write-Host Get-Content -Path "$StdErr"
+                Write-Host " "
+                Write-Host " "
+                Write-Host "******************* OUTPUT LOG ***********************"
+                Write-Host " "
+                Write-Host Get-Content -Path "$StdOut"
+            }
+            $host.SetShouldExit(1)
+        }
     }
+
 
     Write-Host "==================== Downloading Files ==================="
     Start-FileDownload $VCURL -Timeout 60000 -FileName $VCInstaller
@@ -62,47 +109,34 @@ if (-Not (Test-Path $Env:PYTHON)) {
     Write-Host "=============== Installing Requirements =============="
 
     Write-Host "  ---- Installing Stackless 2.7.12150"
-    Start-Process MsiExec.exe -ArgumentList "/I", "$StacklessInstaller", "/quiet", "/passive", "/qn", "/norestart", "TARGETDIR=$Env:PYTHON" -NoNewWindow -Wait
-    Write-Host "       Done."
+    RUN-APP "$StacklessInstaller" "TARGETDIR=$Env:PYTHON"
 
     Write-Host "  ---- Installing Visual C Compiler for Python 2.7"
-    Start-Process MsiExec.exe -ArgumentList "/I", "$VCInstaller", "/quiet", "/passive", "/qn", "/norestart" -NoNewWindow -Wait
-    Write-Host "       Done."
+    RUN-APP "$VCInstaller" ""
 
     Write-Host "  ---- Upgrading pip 9.0.1"
-    Start-Process python -RedirectStandardError "$ModuleOutputFolder\pip 9.0.1.err.log" -RedirectStandardOutput "$ModuleOutputFolder\pip 9.0.1.out.log" -ArgumentList "-m", "pip", "install", "--no-cache-dir", "-U", "pip==9.0.1" -NoNewWindow -Wait
-    Write-Host "       Done."
+    RUN-APP "python" "-m pip install --no-cache-dir -U pip==9.0.1" "$ModuleOutputFolder\pip 9.0.1.err.log" "$ModuleOutputFolder\pip 9.0.1.out.log"
 
     Write-Host "  ---- Upgrading setuptools 40.2.0"
-    Start-Process python -RedirectStandardError "$ModuleOutputFolder\setuptools 40.2.0.err.log" -RedirectStandardOutput "$ModuleOutputFolder\setuptools 40.2.0.out.log" -ArgumentList "-m", "pip", "install", "--no-cache-dir", "-U", "setuptools==40.2.0" -NoNewWindow -Wait
-    Write-Host "       Done."
+    RUN-APP "python" "-m pip install --no-cache-dir -U setuptools==40.2.0" "$ModuleOutputFolder\setuptools 40.2.0.err.log" "$ModuleOutputFolder\setuptools 40.2.0.out.log"
 
     Write-Host "  ---- Installing wxPython 3.0.2.0"
-    Start-Process $WXInstaller -ArgumentList "/VerySilent", "/NoRestart", "/NoCancel", "/SupressMessageBoxes", "/Silent", "/dir=$SitePackages" -NoNewWindow -Wait
-    Write-Host "       Done"
+    RUN-APP $WXInstaller "/dir=$SitePackages"
 
     Write-Host "  ---- Installing py2exe 0.6.9"
-    Start-Process easy_install -RedirectStandardError "$ModuleOutputFolder\py2exe 0.6.9.err.log" -RedirectStandardOutput "$ModuleOutputFolder\py2exe 0.6.9.out.log" -ArgumentList "--always-unzip", "$Py2ExeInstaller" -NoNewWindow -Wait
-    Write-Host "       Done."
+    RUN-APP "easy_install" "--always-unzip $Py2ExeInstaller" "$ModuleOutputFolder\py2exe 0.6.9.err.log" "$ModuleOutputFolder\py2exe 0.6.9.out.log"
 
     # *See Changes* PipInstall "pycrypto 2.6.1" "pycrypto==2.6.1"
     # *See Changes* PipInstall "ctypeslib 0.5.6" "svn+http://svn.python.org/projects/ctypes/trunk/ctypeslib/#ctypeslib=0.5.6"
-    PipInstall "pycryptodome 3.6.6" "pycryptodome==3.6.6" $ModuleOutputFolder
-    PipInstall "wheel 0.29.0" "wheel==0.29.0" $ModuleOutputFolder
-    PipInstall "jinja2 2.8.1" "jinja2==2.8.1" $ModuleOutputFolder
-    PipInstall "sphinx 1.5.6" "sphinx==1.5.6" $ModuleOutputFolder
-    PipInstall "commonmark 0.7.3" "commonmark==0.7.3" $ModuleOutputFolder
-    PipInstall "pillow 3.4.2" "pillow==3.4.2" $ModuleOutputFolder
-    PipInstall "comtypes 1.1.3" "https://github.com/enthought/comtypes/archive/1.1.3.zip" $ModuleOutputFolder
-    PipInstall "paramiko 2.2.1" "paramiko==2.2.1" $ModuleOutputFolder
-    PipInstall "pywin32 223" "pywin32==223" $ModuleOutputFolder
-
-    If (Test-Path $SystemDLL) {
-        Copy-Item $SystemDLL -Destination $Env:PYTHON
-    }
-    If (Test-Path $SysWOWDLL) {
-        Copy-Item $SysWOWDLL -Destination $Env:PYTHON
-    }
+    RUN-APP "pip" "pycryptodome 3.6.6" "pycryptodome==3.6.6" -LogDir $ModuleOutputFolder
+    RUN-APP "pip" "wheel 0.29.0" "wheel==0.29.0" -LogDir $ModuleOutputFolder
+    RUN-APP "pip" "jinja2 2.8.1" "jinja2==2.8.1" -LogDir $ModuleOutputFolder
+    RUN-APP "pip" "sphinx 1.5.6" "sphinx==1.5.6" -LogDir $ModuleOutputFolder
+    RUN-APP "pip" "commonmark 0.7.3" "commonmark==0.7.3" -LogDir $ModuleOutputFolder
+    RUN-APP "pip" "pillow 3.4.2" "pillow==3.4.2" -LogDir $ModuleOutputFolder
+    RUN-APP "pip" "comtypes 1.1.3" "https://github.com/enthought/comtypes/archive/1.1.3.zip" -LogDir $ModuleOutputFolder
+    RUN-APP "pip" "paramiko 2.2.1" "paramiko==2.2.1" -LogDir $ModuleOutputFolder
+    RUN-APP "pip" "pywin32 223" "pywin32==223" -LogDir $ModuleOutputFolder
 
 } else {
     # we are already using a cached version so
